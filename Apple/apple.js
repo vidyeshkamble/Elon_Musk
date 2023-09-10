fab028cc2fda6de44c5401f4f284966649946e51
Vidyesh Kamble <143591563+vidyeshkamble@users.noreply.github.com>
2023-09-10 12:00:52 +0530

    Add files via upload

---
+++ Apple/apple.js
@@ -0,0 +1,9 @@
+function login() {
+    let a = document.getElementById('id').value;
+    let b = document.getElementById('password').value;
+
+    alert("You login Successfully");
+
+    console.log(a);
+    console.log(b);
+}
